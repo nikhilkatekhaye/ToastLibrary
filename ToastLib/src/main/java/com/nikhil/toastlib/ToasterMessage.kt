78192d7b224dce823db43cb7b47921a1b158118c
Nkhil Katekhaye <katekhaye.nikhil@gmail.com>
2021-04-13 14:05:03 +0530

    changes added with new toasts

--- ToastLib/src/main/java/com/nikhil/toastlib/ToasterMessage.kt
+++ ToastLib/src/main/java/com/nikhil/toastlib/ToasterMessage.kt
@@ -1,9 +1,43 @@
 package com.nikhil.toastlib
 
 import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
+
 class ToasterMessage {
+
+    fun failureToast(c: Context?, message: String?) {
+        val inflater: LayoutInflater = LayoutInflater.from(c)
+        val layout: View = inflater.inflate(R.layout.failure_toast, null)
+
+        val text = layout.findViewById<View>(R.id.text) as TextView
+        text.text = message
+
+        val toast = Toast(c)
+        toast.setGravity(Gravity.BOTTOM, 0, 50)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = layout
+        toast.show()
+    }
+
+    fun successToast(c: Context?, message: String?) {
+        val inflater: LayoutInflater = LayoutInflater.from(c)
+        val layout: View = inflater.inflate(R.layout.success_toast, null)
+
+        val text = layout.findViewById<View>(R.id.text) as TextView
+        text.text = message
+
+        val toast = Toast(c)
+        toast.setGravity(Gravity.BOTTOM, 0, 50)
+        toast.duration = Toast.LENGTH_LONG
+        toast.view = layout
+        toast.show()
+    }
+
     fun simpleToast(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/nikhil/toastdemoapp/MainActivity.kt
+++ app/src/main/java/com/nikhil/toastdemoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nikhil.toastdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nikhil.toastlib.ToasterMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var toasterMessage = ToasterMessage()
+        toasterMessage.failureToast(this,"Hello Nikhil")
     }
 }
